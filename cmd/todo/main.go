ea68500b61ca6da9d57a0b4d6f8fa42e5baed164
cyblogerz <pranavajay2002@gmail.com>
2023-07-24 16:25:44 +0530

    updated main

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/cyblogerz/todo-go"
+)
+
+const (
+	todoFile = ".todos.json"
+)
 
 func main() {
-	fmt.Println("Hello , you can do it , dont loose hope my bruv")
+	add := flag.Bool("add", false, "add a new todo")
+	complete := flag.Int("complete", 0, "mark a todo as completed")
+
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("Simple todo")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	default:
+		fmt.Fprintln(os.Stdout, "Command not found")
+		os.Exit(0)
+
+	}
+
 }
--- todo.go
+++ todo.go
@@ -17,10 +17,10 @@ type item struct {
 
 type Todos []item
 
-func(t *Todos) Add(task string){
-	todo:= item{
-		Task: task,
-		Done: false,
+func (t *Todos) Add(task string) {
+	todo := item{
+		Task:      task,
+		Done:      false,
 		CreatedAt: time.Now(),
 	}
 
@@ -28,54 +28,55 @@ func(t *Todos) Add(task string){
 
 }
 
-func (t *Todos) Complete(index int)error{
-	ls:= *t
+func (t *Todos) Complete(index int) error {
+	ls := *t
 
-	if index<=0 || index>len(ls){
+	if index <= 0 || index > len(ls) {
 		return errors.New("invalid index")
 	}
-	ls[index-1].CompletedAt = time.Now();
-	ls[index-1].Done = true;
+	ls[index-1].CompletedAt = time.Now()
+	ls[index-1].Done = true
 
 	return nil
 }
 
-
-func(t *Todos) Delete(index int) error{
-	ls:= *t;
-	if index<=0 || index>len(ls){
+func (t *Todos) Delete(index int) error {
+	ls := *t
+	if index <= 0 || index > len(ls) {
 		return errors.New("valid index")
 	}
-	*t = append(ls[:index-1],ls[index:]...)
+	*t = append(ls[:index-1], ls[index:]...)
 
 	return nil
 }
 
-func (t *Todos) Load(filename string) error{
-	file,err := ioutil.ReadFile(filename)
-	if err!= nil{
-		if errors.Is(err,os.ErrNotExist){
+func (t *Todos) Load(filename string) error {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
 			return nil
 		}
 		return err
 	}
 
-	if len(file) == 0{
+	if len(file) == 0 {
 		return err
 	}
 
-	err =json.Unmarshal(file,t)
-	if err!=nil{
+	err = json.Unmarshal(file, t)
+	if err != nil {
 		return err
 	}
 
 	return nil
 }
 
+func (t *Todos) Store(filename string) error {
+	data, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
 
+	return ioutil.WriteFile(filename, data, 0064)
 
- 
-
-
-
-
+}
