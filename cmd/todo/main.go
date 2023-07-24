47fc9b4b20785a3dd5802398dbbc3e9a18d76578
cyblogerz <pranavajay2002@gmail.com>
2023-07-24 17:55:22 +0530

    Added list functionality

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -15,6 +15,8 @@ const (
 func main() {
 	add := flag.Bool("add", false, "add a new todo")
 	complete := flag.Int("complete", 0, "mark a todo as completed")
+	del := flag.Int("del", 0, "delete a todo")
+	list := flag.Bool("list", false, "list all todos")
 
 	flag.Parse()
 
@@ -45,10 +47,24 @@ func main() {
 			os.Exit(1)
 		}
 
+	case *del > 0:
+		err := todos.Delete(*del)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	case *list:
+		todos.Print()
+
 	default:
 		fmt.Fprintln(os.Stdout, "Command not found")
 		os.Exit(0)
-
 	}
 
 }
